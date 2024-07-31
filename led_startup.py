198f51e62f7dda863ee87559b2702ea41739b7f3
Birdheezy <40696786+Birdheezy@users.noreply.github.com>
2024-07-31 08:15:23 -0600

    Update led_startup.py
    updated starting metar.py for virtual environment

--- led_startup.py
+++ led_startup.py
@@ -1,39 +1,35 @@
-# led_startup.py
-
 import board
 import neopixel
 import time
 import subprocess
+import sys
 
 from config import *
 
 # Initialize the NeoPixel strip
-pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, pixel_order=LED_ORDER, brightness=LED_BRIGHTNESS)
+pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, pixel_order=LED_ORDER, brightness=LED_BRIGH>
 
 def cycle_colors():
-	colors = [COLOR_VFR, COLOR_MVFR, COLOR_IFR, COLOR_LIFR]
-	for color in colors:
-		# Set all LEDs to the current color
-		pixels.fill(color)
-		pixels.show()
-		time.sleep(.7)  # Adjust the duration for each color
+    colors = [COLOR_VFR, COLOR_MVFR, COLOR_IFR, COLOR_LIFR]
+    for color in colors:
+        # Set all LEDs to the current color
+        pixels.fill(color)
+        pixels.show()
+        time.sleep(0.7)  # Adjust the duration for each color
 
 def main():
-	# Turn on all LEDs
-	pixels.fill((255, 255, 255))  # White
-	pixels.show()
-	time.sleep(.7)  # Adjust the duration LEDs stay on
+    # Turn on all LEDs
+    pixels.fill((255, 255, 255))  # White
+    pixels.show()
+    time.sleep(0.7)  # Adjust the duration LEDs stay on
 
-	# Cycle through colors
-	cycle_colors()
+    # Cycle through colors
+    cycle_colors()
 
-	# Turn off all LEDs
-	pixels.show()
+    print("Starting metar.py script...")
+    subprocess.run(["sudo", "/home/pi/metar/bin/python3", "metar.py"])  # Use the alias di>
 
-	print("Starting metar.py script...")
-	subprocess.run(["python3", "metar.py"]) 
-
-	sys.exit()
+    sys.exit()
 
 if __name__ == "__main__":
-	main()
+    main()
